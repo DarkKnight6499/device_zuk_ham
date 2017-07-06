baf53c4a415ff5c4182a243335f2c6df64350f26
root <root@Anush01065.ljom3vhs4eie5lnyo53rylizva.tx.internal.cloudapp.net>
2017-07-06 06:24:05 +0000

    ham: Disable EAP-SIM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,8 +176,8 @@ TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
 # Wifi - EAP-SIM
-CONFIG_EAP_PROXY                 := qmi
-CONFIG_EAP_PROXY_DUAL_SIM        := true
+#CONFIG_EAP_PROXY                 := qmi
+#CONFIG_EAP_PROXY_DUAL_SIM        := true
 
 # Inherit from QC proprietary
 ifneq ($(QCPATH),)
